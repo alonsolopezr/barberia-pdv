6dd16a75249824585c9edd2175efa98a0b272fa4
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2021-02-15 19:56:41 -0700

    se creo proyecot de console app
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

---
+++ barberia-pdv-code/barberia-pdv-code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace barberia_pdv_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
